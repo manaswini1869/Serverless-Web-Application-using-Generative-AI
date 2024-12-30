536f9f38ab22675f5dd7b3623cdf59e4a9996760
Manaswini1869 <goginenim18069@gmail.com>
2024-12-30 17:42:29 +0530

    feat(genai): final app

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,15 +3,22 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
+specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a.query()
+  .arguments({ ingredients: a.string().array() })
+  .returns(a.ref("BedrockResponse"))
+  .authorization((allow) => [allow.authenticated()])
+  .handler(
+    a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrock"})
+  ),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +26,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
 
@@ -28,7 +38,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
@@ -37,7 +47,6 @@ cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 "use client"
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-
 const client = generateClient<Schema>() // use this Data client for CRUDL requests
 */
 
